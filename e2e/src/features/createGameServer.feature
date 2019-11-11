29c826d50fbccb9060e5f3fc3d842930a72d1568
Leonhard Gahr <leonhard.gahr@gmail.com>
2019-11-11 10:57:22 +0100

    add cucumber feature files and end to end tests for them

---
+++ e2e/src/features/createGameServer.feature
@@ -0,0 +1,16 @@
+Feature: Create game server
+  Scenario: User creates a game server
+    Given I am on the dashboard page
+    And I click on the "Create game server" button
+    When I fill the form with the required information
+    And I click on the "Create" button
+    Then the game server is created
+    And I am redirected to the configuration page for the game server
+
+  Scenario: User cancels creation of a game server
+    Given I am on the dashboard page
+    And I click on the "Create game server" button
+    When I may or may not fill the form with the required information
+    And I click the "Cancel" button
+    Then the game server is not created
+    And I stay on the dashboard page
---
+++ e2e/src/steps/app.steps.ts
@@ -0,0 +1,42 @@
+import {Given, Then, When} from 'cucumber';
+import {expect} from 'chai';
+
+Given(/^I am on the dashboard page$/, _ => {
+
+});
+Given(/^I click on the "([^"]*)" button$/, button => {
+
+});
+When(/^I fill the form with the required information$/, _ => {
+
+});
+Then(/^the game server is created$/, _ => {
+  expect('hello').to.equal('goodbye');
+});
+Then(/^I am redirected to the configuration page for the game server$/, _ => {
+
+});
+When(/^I may or may not fill the form with the required information$/, _ => {
+
+});
+When(/^I click the "([^"]*)" button$/, button => {
+
+});
+Then(/^the game server is not created$/, _ => {
+
+});
+Then(/^I stay on the dashboard page$/, _ => {
+
+});
+Given(/^I am on the detail page for the game server$/, _ => {
+
+});
+Then(/^the game server is reconfigured$/, _ => {
+
+});
+Then(/^I am redirected to the detail page for the game server$/, _ => {
+
+});
+Then(/^the game server is not reconfigured$/, _ => {
+
+});
