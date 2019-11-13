7e1054d423b78212f89486ad92ed4e0046805b28
Leonhard Gahr <leonhard.gahr@gmail.com>
2019-11-13 13:10:24 +0100

    set style engine to scss

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,7 +11,7 @@ export interface Tile {
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   tiles: Tile[] = [
