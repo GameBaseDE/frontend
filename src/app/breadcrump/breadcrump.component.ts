bbf6f093e36ce79900cbc51fad9d0b9ab992835f
Leonhard Gahr <leonhard.gahr@gmail.com>
2019-11-14 13:36:41 +0100

    optimize layout; adapt 404 page to show useful content; add titles to pages;

--- src/app/breadcrump/breadcrump.component.ts
+++ src/app/breadcrump/breadcrump.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrump',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+  }
 
   ngOnInit() {
   }
--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private titleService: Title) {
+  }
 
   ngOnInit() {
+    this.titleService.setTitle('GameBase | Error 404');
   }
 
 }
