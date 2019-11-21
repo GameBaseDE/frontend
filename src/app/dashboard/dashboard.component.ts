f976a9cc4337bf4ad01082255c054fe094944db0
Leonhard Gahr <leonhard.gahr@gmail.com>
2019-11-21 10:49:08 +0100

    GB-48: design changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,9 +17,90 @@ export class DashboardComponent implements OnInit {
         id: '1',
         image: 'minecraft',
         ports: [1, 2, 3],
-        slots: 12
+        slots: 12,
+        status: 'Running',
       }
     ];
+    this.randomizeStatus();
+  }
+
+  /**
+   * Button click event handler for the restart GameServer button
+   *
+   * @param event the click event
+   * @param id the GameServer id to restart
+   */
+  restartServerEvent(event: Event, id: string) {
+    event.preventDefault();
+    if (!this.restartPossible(id)) {
+      return;
+    }
+    console.log(`Restarting GameServer #${id}`);
+  }
+
+  /**
+   * Button click event handler for the start GameServer button
+   *
+   * @param event the click event
+   * @param id the GameServer id to restart
+   */
+  startServerEvent(event: Event, id: string) {
+    event.preventDefault();
+    if (!this.startPossible(id)) {
+      return;
+    }
+    console.log(`Starting GameServer #${id}`);
+  }
+
+  /**
+   * Button click event handler for the stop GameServer button
+   *
+   * @param event the click event
+   * @param id the GameServer id to restart
+   */
+  stopServerEvent(event: Event, id: string) {
+    event.preventDefault();
+    if (!this.stopPossible(id)) {
+      return;
+    }
+    console.log(`Stopping GameServer #${id}`);
+  }
+
+  /**
+   * Returns whether a restart is possible for a specific GameServer
+   * @param id the id of the GameServer to check
+   */
+  restartPossible(id: string) {
+    const gameServer = this.gameServers.find(value => value.id === id);
+    return gameServer.status === 'Running';
+  }
+
+  /**
+   * Returns whether a restart is possible for a specific GameServer
+   * @param id the id of the GameServer to check
+   */
+  stopPossible(id: string) {
+    const gameServer = this.gameServers.find(value => value.id === id);
+    return gameServer.status === 'Running';
+  }
+
+  /**
+   * Returns whether a restart is possible for a specific GameServer
+   * @param id the id of the GameServer to check
+   */
+  startPossible(id: string) {
+    const gameServer = this.gameServers.find(value => value.id === id);
+    return gameServer.status === 'Stopped' || gameServer.status === 'Error';
+  }
+
+  // test function
+  randomizeStatus() {
+    const status = ['Running', 'Restarting', 'Stopped', 'Error'];
+    setInterval(() => {
+      this.gameServers.forEach(value => {
+        value.status = status[Math.floor(Math.random() * status.length)];
+      });
+    }, 1000);
   }
 
   ngOnInit() {
--- src/app/rest/response/GameServerStatus.ts
+++ src/app/rest/response/GameServerStatus.ts
@@ -1,6 +1,7 @@
 export class GameServerStatus {
   id: string;
   image: string;
+  status: string;
   ports: number[];
   slots: number;
 }
