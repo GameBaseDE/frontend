2d52369146643edfe854bee12b571b36f57c87a7
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-05-14 11:54:04 +0200

    GB-101: fix tests

--- src/app/createserver/createserver.component.spec.ts
+++ src/app/createserver/createserver.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CreateServerWizardComponent } from './createserver.component';
+import {CreateServerWizardComponent} from './createserver.component';
+import {NbCardModule, NbRadioModule} from '@nebular/theme';
+import {FormsModule} from '@angular/forms';
 
 describe('CreateServerWizardComponent', () => {
   let component: CreateServerWizardComponent;
@@ -8,9 +10,10 @@ describe('CreateServerWizardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateServerWizardComponent ]
+      declarations: [CreateServerWizardComponent],
+      imports: [NbCardModule, FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
