702b8db668db26012af5ec0c01015087f73d0fdb
Humenius <contact@humenius.me>
2020-05-14 12:50:03 +0200

    GB-101: Add live input validation for port allocation

--- src/app/createserver/createserver.component.ts
+++ src/app/createserver/createserver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameServerDeployTemplate } from '../rest-client/models';
 
 @Component({
   selector: 'app-createserverwizard',
@@ -14,13 +15,26 @@ export class CreateServerWizardComponent implements OnInit {
     { value: 'always', label: 'Always' },
   ];
 
+  deployTemplate: GameServerDeployTemplate = {
+    image: null,
+    ownerId: null
+  }
+
   dockerImage = '';
 
   startupArgs = '';
 
-  memoryAlloc = '';
+  memoryAlloc = {
+    value: '',
+    hasError: false
+  };
 
-  portAlloc = '';
+  portAlloc = {
+    value: '',
+    hasError: false
+  };
+
+  private static numericListRegExp: RegExp = /^\d{1,5}(,\d{1,5})*$/gm;
 
   constructor() { }
 
@@ -33,5 +47,31 @@ export class CreateServerWizardComponent implements OnInit {
     console.log('>> Startup args: ' + this.startupArgs);
     console.log('>> Memory alloc: ' + this.memoryAlloc);
     console.log('>> Port alloc: ' + this.portAlloc);
+  }
+
+  onInputMemoryAlloc(event: any): void {
+    // let value = event.target.value;
+    // if (this.isNumericList(value) && this.isValueInValidRange(value)) {
+    //   console.log(value + " is a numericLis")
+    // }
+  }
+
+  onInputPortAlloc(event: any): void {
+    let value = event.target.value;
+    console.log("> onInputPortAlloc(" + value + ")");
+    console.log(">> isNumericList: " + this.isNumericList(value));
+    value.split(",").forEach(element => {
+      if (!(element.length > 0)) { return; }
+      console.log(">> isValueInValidRange(" + element + "): " + this.isValueInValidRange(element));
+    });
+  }
+
+  private isNumericList(input: string): boolean {
+    let result = input.match(CreateServerWizardComponent.numericListRegExp);
+    return result !== null && result.length > 0;
+  }
+
+  private isValueInValidRange(input: any): boolean {
+    return input >= 1024 && input <= 65565;
   }
 }
