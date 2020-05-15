dfc926b97cc5bc02f7fea7bc69595ecc5e9c765d
Humenius <contact@humenius.me>
2020-05-15 11:27:09 +0200

    GB-101: Add port allocation input validation test

--- src/app/createserver/createserver.component.spec.ts
+++ src/app/createserver/createserver.component.spec.ts
@@ -56,4 +56,53 @@ describe('CreateServerWizardComponent', () => {
     expect(actual).toEqual(expected);
     expect(component.memoryAlloc.hasError).toBeFalse();
   });
+
+  it('should validate port allocation and display error', () => {
+    let expected = '1354987,,,';
+    let input = fixture.debugElement.query(By.css('input[name="input-port-allocation"]'));
+    let element = input.nativeElement;
+
+    expect(element).toBeTruthy();
+
+    element.value = expected;
+    element.dispatchEvent(new Event('input'));
+
+    let actual = component.portAlloc.rawValue;
+    expect(actual).toEqual(expected);
+    expect(component.portAlloc.hasError).toBeTrue();
+  });
+
+  it('should validate port allocation with invalid port values, display error and parse into faulty values properly', () => {
+    let expected = '1,5,54,5123';
+    let expectedFaulty = ['1', '5', '54'];
+    let input = fixture.debugElement.query(By.css('input[name="input-port-allocation"]'));
+    let element = input.nativeElement;
+
+    expect(element).toBeTruthy();
+
+    element.value = expected;
+    element.dispatchEvent(new Event('input'));
+
+    let actual = component.portAlloc.rawValue;
+    expect(actual).toEqual(expected);
+    expect(component.portAlloc.hasError).toBeTrue();
+    expect(component.portAlloc.faultyValues).toEqual(expectedFaulty);
+  });
+
+  it('should validate port allocation, display no error and parse values properly', () => {
+    let expected = '1024,65565';
+    let expectedParsed = ['1024', '65565'];
+    let input = fixture.debugElement.query(By.css('input[name="input-port-allocation"]'));
+    let element = input.nativeElement;
+
+    expect(element).toBeTruthy();
+
+    element.value = expected;
+    element.dispatchEvent(new Event('input'));
+
+    let actual = component.portAlloc.rawValue;
+    expect(actual).toEqual(expected);
+    expect(component.portAlloc.hasError).toBeFalse();
+    expect(component.portAlloc.parsedValues).toEqual(expectedParsed);
+  });
 });
