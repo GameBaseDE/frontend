7ec062cbef4967910b9a589f192f0c5c80c48ad5
Humenius <contact@humenius.me>
2020-05-26 09:32:05 +0200

    Add EnumUtils.getKeyByValue in global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,7 +1,26 @@
 export class Constants {
-    public static dummyOwnerId: string = 'c6a4f782-9aa2-11ea-bb37-0242ac130002';
+  public static dummyOwnerId: string = 'c6a4f782-9aa2-11ea-bb37-0242ac130002';
 }
 
 export class StringUtils {
-    public static isEmptyOrNull(value: string): boolean { return !(value !== null && (value.length > 0)); }
+  public static isEmptyOrNull(value: string): boolean { return !(value !== undefined && (value.length > 0)); }
+}
+
+export class EnumUtils {
+  /**
+   * Get value by key through enum key iteration as no reverse mapping for enums are offered.
+   * @param enum$ Enum whose keys are to be iterated
+   * @param value Value of key to be found
+   */
+  public static getKeyByValue(enum$: any, value: any): any {
+    let keys = Object.keys(enum$);
+
+    for (const key in keys) {
+      if (key === value) {
+        return key;
+      }
+    }
+
+    return undefined;
+  }
 }
