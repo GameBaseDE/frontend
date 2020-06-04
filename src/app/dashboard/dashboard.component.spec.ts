2513877afa7cd6fda048ee30e6b87187b2eed7b2
Humenius <contact@humenius.me>
2020-06-04 16:06:15 +0200

    GB-115: Add DashboardComponent tests for accordion items

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -10,6 +10,7 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {ApiModule} from '../rest-client/api.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {By} from '@angular/platform-browser';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -20,7 +21,7 @@ describe('DashboardComponent', () => {
       declarations: [DashboardComponent],
       imports: [NbAccordionModule, NbIconModule, NbAlertModule, BrowserAnimationsModule, NbCardModule, NgxEchartsModule,
         NbThemeModule.forRoot(), ToastrModule.forRoot(), NbStepperModule, FormsModule, NbInputModule, HttpClientModule, AppRoutingModule,
-        ApiModule.forRoot({rootUrl: environment.restApiURL})]
+        ApiModule.forRoot({rootUrl: environment.mockAPIURL})]
     })
       .compileComponents();
   }));
@@ -33,5 +34,56 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display game server accordion with at least one entry', () => {
+    const selector = 'nb-accordion > nb-accordion-item';
+    const accordion = fixture.debugElement.query(By.css(selector));
+    const element = accordion.nativeElement;
+
+    expect(element).toBeTruthy(`${selector} could not be found!`);
+
+    const headerSelector = 'nb-accordion-item-header';
+    const header = fixture.debugElement.query(By.css(headerSelector)).nativeElement;
+
+    expect(header).toBeTruthy(`${headerSelector} could not be found`);
+  });
+
+  it('should expand game server details if corresponding accordion item is clicked', () => {
+    const selector = 'nb-accordion > nb-accordion-item > nb-accordion-item-header';
+    const accordion = fixture.debugElement.query(By.css(selector));
+    const element = accordion.nativeElement;
+
+    expect(element).toBeTruthy(`${selector} could not be found!`);
+
+    element.dispatchEvent(new Event('click'));
+
+    expect(element.expanded).toBeTruthy(`${element} is not expanded although clicked.`);
+  });
+
+  it('should contain details inside expanded accordion item', () => {
+    const selector = 'nb-accordion > nb-accordion-item > nb-accordion-item-body';
+    const accordion = fixture.debugElement.query(By.css(selector));
+    const element = accordion.nativeElement;
+
+    expect(element).toBeTruthy(`${selector} could not be found!`);
+
+    const descriptionTitle = element.debugElement.query(By.css('p.title')).nativeElement;
+    expect(descriptionTitle).toBeTruthy(`${descriptionTitle} could not be found!`);
+
+    expect(descriptionTitle.value).toEqual('Description', `${descriptionTitle.value} != description`);
+  });
+
+  it('should display startup arguments in a disabled text box', () => {
+    const selector = 'nb-accordion > nb-accordion-item > nb-accordion-item-body';
+    const accordion = fixture.debugElement.query(By.css(selector));
+    const element = accordion.nativeElement;
+
+    expect(element).toBeTruthy(`${selector} could not be found!`);
+
+    const descriptionTitle = element.debugElement.query(By.css('input')).nativeElement;
+    expect(descriptionTitle).toBeTruthy(`${descriptionTitle} could not be found!`);
+
+    expect(descriptionTitle.readonly).toEqual('', `${descriptionTitle.value} != ''`);
   });
 });
