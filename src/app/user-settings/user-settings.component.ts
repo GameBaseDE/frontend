1f5e4529b63bf030f6e031a1ec7addbc8d08e641
Humenius <contact@humenius.me>
2020-06-19 12:19:05 +0200

    GB-129: First implementation of view and controller

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {GameserverService} from '../rest-client/services/gameserver.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NbThemeService} from '@nebular/theme';
 
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.scss']
+  styleUrls: ['./user-settings.component.scss', '../serverconfiguration/serverconfiguration.component.scss']
 })
 export class UserSettingsComponent implements OnInit {
+  generalDetails = {
+    username: undefined,
+    emailAddress: undefined,
+    password: {
+      old: undefined,
+      $new: undefined,
+      repeat: undefined
+    },
+    gravatarEmail: undefined
+  };
 
-  constructor() { }
+  constructor(
+    private toastr: ToastrService,
+    private gameServerService: GameserverService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  cancel() {
+
+  }
+
+  apply() {
+
+  }
 }
