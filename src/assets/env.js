d77d3289e217e289055058293596a6f54a032a82
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-06-22 18:31:21 +0200

    make docker ready; add dynamic env-vars via docker;

---
+++ src/assets/env.js
@@ -0,0 +1,6 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["restApiURL"] = "https://api.game-base.de";
+})(this);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  restApiURL: 'https://api.game-base.de'
+  // tslint:disable-next-line
+  restApiURL: window['env']['restApiURL'] || 'https://api.game-base.de'
 };
