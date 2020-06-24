403136d482f39388aa48be72bbb8605a395068ee
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-06-24 10:48:43 +0200

    add dynamic backend urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // tslint:disable-next-line
-  restApiURL: window['env']['restApiURL'] || 'https://api.game-base.de'
+  restApiURL: window['env']['restApiURL'] || `${location.protocol}//${location.hostname}`
 };
