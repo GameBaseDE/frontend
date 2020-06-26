7dc830e56b7a1af4cf42bdb93403b0c551e42a19
Humenius <contact@humenius.me>
2020-06-26 21:20:17 +0200

    GB-129: Import missing modules for UserSettingsComponent Karma test

--- src/app/user-settings/user-settings.component.spec.ts
+++ src/app/user-settings/user-settings.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserSettingsComponent } from './user-settings.component';
+import {ServerConfigurationComponent} from '../serverconfiguration/serverconfiguration.component';
+import {GameserverService} from '../rest-client/services/gameserver.service';
+import {GameserverMockService} from '../mock/mockapi/services/gameserver-mock.service';
+import {NbCardModule, NbInputModule, NbRadioModule, NbThemeModule} from '@nebular/theme';
+import {FormsModule} from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
+import {HttpClientModule} from '@angular/common/http';
+import {MockApiModule} from '../mock/mockapi/mock-api.module';
+import {AppRoutingModule} from '../app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NbAuthModule, NbDummyAuthStrategy} from '@nebular/auth';
 
 describe('UserSettingsComponent', () => {
   let component: UserSettingsComponent;
@@ -8,9 +19,31 @@ describe('UserSettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserSettingsComponent ]
+      declarations: [UserSettingsComponent],
+      providers: [
+        { provide: GameserverService, useClass: GameserverMockService }
+      ],
+      imports: [
+        NbCardModule,
+        FormsModule,
+        ToastrModule.forRoot(),
+        HttpClientModule,
+        MockApiModule, // Mock Module
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot(),
+        FormsModule,
+        NbInputModule,
+        NbRadioModule,
+        NbAuthModule.forRoot({
+          strategies: [NbDummyAuthStrategy.setup({
+            alwaysFail: false,
+            name: 'email'
+          })]
+        })
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
