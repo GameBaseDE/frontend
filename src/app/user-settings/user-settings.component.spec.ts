2e21608d00bdc2432215e3cb79e83969ed2de279
Humenius <contact@humenius.me>
2020-06-29 17:38:16 +0200

    GB-129: Add error test for UserSettingsComponent

--- src/app/user-settings/user-settings.component.spec.ts
+++ src/app/user-settings/user-settings.component.spec.ts
@@ -4,7 +4,7 @@ import { UserSettingsComponent } from './user-settings.component';
 import {ServerConfigurationComponent} from '../serverconfiguration/serverconfiguration.component';
 import {GameserverService} from '../rest-client/services/gameserver.service';
 import {GameserverMockService} from '../mock/mockapi/services/gameserver-mock.service';
-import {NbCardModule, NbInputModule, NbRadioModule, NbThemeModule} from '@nebular/theme';
+import {NbCardModule, NbInputModule, NbRadioModule, NbThemeModule, NbUserModule} from '@nebular/theme';
 import {FormsModule} from '@angular/forms';
 import {ToastrModule} from 'ngx-toastr';
 import {HttpClientModule} from '@angular/common/http';
@@ -12,6 +12,7 @@ import {MockApiModule} from '../mock/mockapi/mock-api.module';
 import {AppRoutingModule} from '../app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NbAuthModule, NbDummyAuthStrategy} from '@nebular/auth';
+import {By} from '@angular/platform-browser';
 
 describe('UserSettingsComponent', () => {
   let component: UserSettingsComponent;
@@ -25,6 +26,7 @@ describe('UserSettingsComponent', () => {
       ],
       imports: [
         NbCardModule,
+        NbUserModule,
         FormsModule,
         ToastrModule.forRoot(),
         HttpClientModule,
@@ -55,4 +57,30 @@ describe('UserSettingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display error if new password and repeated new password does not equal', () => {
+    const selectors = {
+      newPassword: 'input[name=input-new-password]',
+      repeatPassword: 'input[name=input-repeat-password]'
+    }
+
+    const inputContent = ['asdf', 'not_asdf'];
+
+    const newPasswordElement = fixture.debugElement.query(By.css(selectors.newPassword)).nativeElement;
+    const repeatPasswordElement = fixture.debugElement.query(By.css(selectors.repeatPassword)).nativeElement;
+
+    expect(newPasswordElement).toBeTruthy(`${selectors.newPassword} could not be found!`);
+    expect(repeatPasswordElement).toBeTruthy(`${selectors.repeatPassword} could not be found!`);
+
+    dispatchInput(newPasswordElement, inputContent[0]);
+    dispatchInput(repeatPasswordElement, inputContent[1]);
+
+    expect(component.generalDetails.password.repeat.error.hasError).toBeTruthy(`Error is not displayed although ${inputContent[0]} and ${inputContent[1]} do not equal`);
+  });
 });
+
+function dispatchInput(element: any, text: string) {
+  element.value = text;
+  element.dispatchEvent(new Event('input'));
+  element.dispatchEvent(new Event('keyup'));
+}
